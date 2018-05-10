4162df2ef304723885b2b64b976d25702c2fbcb4
HiimFinest <31751323+HiimFinest@users.noreply.github.com>
2018-05-10 13:12:53 -0700

    OG
    original code

---
+++ dice.py
@@ -0,0 +1,17 @@
+import random
+
+
+
+def dieroller(y):
+        print("you have rolled a %s" % random.randint(1,6), end ="")
+        for item in range(0, int(y)-2):
+            print(", %s" %  random.randint(1,6), end ="")
+        print(', and a %s' % random.randint(1,6))
+            
+            
+print ('Welcome to the python dice simulator!')
+
+user_input = input('Enter the number of dice you would like to roll as an integer and hit ENTER: ')
+
+dieroller(user_input)
+
